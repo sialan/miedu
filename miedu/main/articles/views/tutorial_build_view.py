e1e0f24dbf92795a379d86620e7ec409ccc30781
Alan Si <si.alan090@gmail.com>
2013-03-28 01:14:22 -0700

    changed all() to filter querysets

--- miedu/main/articles/views/tutorial_build_view.py
+++ miedu/main/articles/views/tutorial_build_view.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from articles.models import Article
 
 def article(request):
-    article_data_model = Article.objects.get(id=4)
+    article_data_model = Article.objects.get(id=3)
     context = {'article': article_data_model}
     return render(request, 'templates/build.html', context)
