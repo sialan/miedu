b44cf4172681d6d5b27574711905ca1d14ca1390
Alan Si <si.alan090@gmail.com>
2013-03-30 13:50:58 -0700

    removed sensitive login data from settings

--- miedu/settings/dev.py
+++ miedu/settings/dev.py
@@ -1,40 +1,35 @@
-# python manage.py syncdb ->, python manage.py syncdb --settings=settings.development
 from common import *
 from os.path import join, normpath
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = normpath(join(SITE_ROOT, 'media'))
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
-DIRECTORY = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'static'
+STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
 
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+# URL prefix for static files./
 STATIC_URL = '/static/'
 
+# URL prefix for admin static files -- CSS, JavaScript and images. Make sure to use a trailing slash.
 ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
 
-FILEBROWSER_MEDIA_ROOT = MEDIA_ROOT 
-FILEBROWSER_MEDIA_URL = MEDIA_URL 
-FILEBROWSER_STATIC_ROOT = STATIC_ROOT 
-FILEBROWSER_STATIC_URL = STATIC_URL 
+FILEBROWSER_MEDIA_ROOT = normpath(MEDIA_ROOT)
+FILEBROWSER_MEDIA_URL = normpath(MEDIA_URL)
+FILEBROWSER_DIRECTORY = ''
+FILEBROWSER_STATIC_ROOT = normpath(STATIC_ROOT)
+FILEBROWSER_STATIC_URL = normpath(STATIC_URL)
 
-URL_FILEBROWSER_MEDIA = STATIC_URL + "filebrowser/"
-PATH_FILEBROWSER_MEDIA = STATIC_ROOT + "filebrowser/"
-URL_TINYMCE = STATIC_URL + "tiny_mce/"
-PATH_TINYMCE = STATIC_ROOT + "tiny_mce/"
+FILEBROWSER_URL_FILEBROWSER_MEDIA = join(STATIC_URL, "filebrowser/")
+FILEBROWSER_PATH_FILEBROWSER_MEDIA = join(STATIC_ROOT, "filebrowser")
+FILEBROWSER_URL_TINYMCE = normpath(join(STATIC_URL, "tiny_mce/jscripts/tiny_mce/"))
+FILEBROWSER_PATH_TINYMCE = normpath(join(STATIC_ROOT, "tiny_mce/jscripts/tiny_mce/"))
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -44,16 +39,12 @@ STATICFILES_DIRS = (
     'miedu/static',
 )
 
-AUTHENTICATION_BACKENDS = (
-    ('django.contrib.auth.backends.ModelBackend'),
-)
-
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql', 'mysql', etc.
-        'NAME': 'miedu_dev',                      # Or path to database file if using sqlite3.
+        'NAME': join(SITE_ROOT, 'miedu_dev_v1',                      # Or path to database file if using sqlite3.
         'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
---
+++ miedu/settings/secret_template.py
@@ -0,0 +1,15 @@
+# TODO: change variables as needed for individual deployment
+# common secret variables for deployment
+aws_storage_bucket_name = '<insert_your_s3_bucket_name>'
+aws_access_key_id = '<insert_your_aws_access_key>'
+aws_secret_access_key = '<insert_your_aws_secret_key>'
+
+# secret variables for staging
+staging_database_name = '<insert_the_name_of_your_staging_database>'
+staging_database_user = '<insert_the_user_of_your_staging_database>'
+staging_database_password = '<insert_the_password_of_your_staging_database>'
+
+# secret variables for production
+production_database_name = '<insert_the_name_of_your_production_database>'
+production_database_user = '<insert_the_user_of_your_production_database>'
+production_database_password = '<insert_the_password_of_your_production_database>'
