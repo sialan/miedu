f71f0ab601a08d258b4b2167c53e18cdb74d5f2d
Alan Si <si.alan090@gmail.com>
2013-04-01 14:45:11 -0700

    fixed indentation issue in campaign views

--- miedu/main/campaigns/views/campaign_list_view.py
+++ miedu/main/campaigns/views/campaign_list_view.py
@@ -4,16 +4,16 @@ from campaigns.models import Campaign
 
 def campaigns(request):
     blueprint_data_collection = Campaign.objects.all()
- 	paginator = Paginator(blueprint_data_collection, 20)
- 	page = request.GET.get('page')
+    paginator = Paginator(blueprint_data_collection, 20)
+    page = request.GET.get('page')
 
- 	try:
+    try:
         context = {'blueprint_list': paginator.page(page)}
     except PageNotAnInteger:
-    	# If page is not an integer, deliver first page.
-    	context = {'blueprint_list': paginator.page(1)}
+       # If page is not an integer, deliver first page.
+       context = {'blueprint_list': paginator.page(1)}
     except EmptyPage:
-    	# If page is out of range (e.g. 9999), deliver last page of results.
+        # If page is out of range (e.g. 9999), deliver last page of results.
         context = {'blueprint_list': paginator.page(paginator.num_pages)}
-        
+
     return render(request, 'blueprints.html', context)
