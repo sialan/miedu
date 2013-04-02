d88e6c08c25b50e4ec3b668c790a484b09f6ef15
Alan Si <si.alan090@gmail.com>
2013-04-01 21:09:33 -0700

    updated blog and blogpost views

--- miedu/main/blogs/views/blog_view.py
+++ miedu/main/blogs/views/blog_view.py
@@ -4,4 +4,4 @@ from blogs.models import Post
 def blog(request):
     post_data_collection = Post.objects.all().order_by('-created')
     context = {'posts': post_data_collection}
-    return render(request, 'templates/blog.html', context)
+    return render(request, 'blog.html', context)
--- miedu/main/blogs/views/post_view.py
+++ miedu/main/blogs/views/post_view.py
@@ -4,4 +4,4 @@ from blogs.models import Post
 def post(request, post_id):
     post_data_model = Post.objects.get(id=post_id)
     context = {'post': post_data_model}
-    return render(request, 'templates/post.html', context)
+    return render(request, 'post.html', context)
