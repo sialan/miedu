c6428d5705af871992279bf2ab8bd4c4856ec226
Alan Si <si.alan090@gmail.com>
2013-04-01 23:53:29 -0700

    added form structure to blueprint view

--- miedu/main/campaigns/forms.py
+++ miedu/main/campaigns/forms.py
@@ -1,31 +1,18 @@
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django import forms
 from campaigns.models import Campaign
 
 class CampaignCreationForm(forms.ModelForm):
     """A form for creating new users. Includes all the required
     fields, plus a repeated password."""
-    
+
     class Meta:
         model = Campaign
-        fields = ('first_name', 'last_name', 'email',)
-        widgets = { 'name': forms.TextInput(attrs={'class': "input-block-level"})}
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
 
     def save(self, commit=True):
         # Save the provided password in hashed format
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        blueprint = super(CampaignCreationForm, self).save(commit=False)
         if commit:
-            user.username = user.email
-            user.save()
+            blueprint.save()
         return user
 
 
--- miedu/main/campaigns/views/campaign_new_view.py
+++ miedu/main/campaigns/views/campaign_new_view.py
@@ -3,16 +3,17 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from campaigns.models import Campaign
+from campaigns.forms import CampaignCreationForm
 from django.views.decorators.csrf import csrf_protect
 
 @csrf_protect
 def campaign(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CampaignCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
             return HttpResponseRedirect(reverse('home'))
     else:
-        form = UserCreationForm()
+        form = CampaignCreationForm()
         context = {"form": form}
-        return render(request, "signup.html", context)
+        return render(request, "new_blueprint.html", context)
